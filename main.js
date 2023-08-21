cd18d791a7d7e49487c1daa564459c0a27d9a01a
marlon <marlon.mosquerar@gmail.com>
2023-08-21 11:45:27 -0500

    inicio maquetacion de la app

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const city = (name) =>`http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=1&appid=6d38995b3a17ddfebcd1527c2a264c29&lang=es`;
 const datosActuales = (lat,lon) =>`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=6d38995b3a17ddfebcd1527c2a264c29&lang=es&units=metric`;
-const datosDia = (lat,lon) => `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=6d38995b3a17ddfebcd1527c2a264c29&lang=es&units=metric`
+const datosHora = (lat,lon) => `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=6d38995b3a17ddfebcd1527c2a264c29&lang=es&units=metric&cnt=3`
 
 const inputCity = document.getElementById('city');
 const lat = document.getElementById('lat');
@@ -8,35 +8,23 @@ const lon = document.getElementById('lon');
 const clima = document.getElementById('clima');
 const temperatura = document.getElementById('temperatura');
 
-async function pronosticoActual (){
+async function pronosticoActual (lat,lon){
     const res = await fetch(datosActuales(lat,lon));
     const data = await res.json();
-   console.log(data)
+   console.log('Pronostico Actual:',data)
 
    clima.innerText = '';
    temperatura.innerText = '';
    clima.innerText = data.weather[0].main;
    temperatura.innerText = data.main.temp;
 };
-async function pronosticoDia (){
-    const res = await fetch('http://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=6d38995b3a17ddfebcd1527c2a264c29&units=metric');
+async function pronosticoHora (lat,lon){
+    const res = await fetch(datosHora(lat,lon));
     const data = await res.json();
-    console.log(data)
-
-    
-    for (let dia   in data.list){
-    
-        var timestamp = data.list[dia].dt;  // Ejemplo de un timestamp en tiempo Unix
-        var dateObject = new Date(timestamp * 1000);  // JavaScript usa milisegundos
-    
-    console.log(dateObject.toLocaleString());  // convertir a una cadena legible
-        
-    }
-   
-
-
-
-
+    console.log('pronostico hora :' ,data)
+    // var timestamp = data.list[dia].dt;  // Ejemplo de un timestamp en tiempo Unix
+    // var dateObject = new Date(timestamp * 1000);  // JavaScript usa milisegundos
+    // console.log(dateObject.toLocaleString());  // convertir a una cadena legible
 }
 
 async function ciudades (name){   // cosumo api de geolocalizacion
@@ -52,6 +40,8 @@ async function ciudades (name){   // cosumo api de geolocalizacion
     lat.innerText=data[0].lat;
     lon.innerText=data[0].lon;
     pronosticoActual (data[0].lat,data[0].lon);
+    pronosticoHora (data[0].lat,data[0].lon)
+    console.log('Ciudad :',data);
 };
 
 // Detecta cuando dentro del input se preciona  Enter
@@ -66,5 +56,5 @@ inputCity.addEventListener('keydown',(event) =>{
 
 
 
-// pronosticoActual(33.44,-94.04);
- pronosticoDia();
+
+//  pronosticoHora();
