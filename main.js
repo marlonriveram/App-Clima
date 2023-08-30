393aafb5e86960b498f45a60c273fb2d439ded56
marlon <marlon.mosquerar@gmail.com>
2023-08-30 16:49:06 -0500

    a la app se le agrago un input en un aside desplegable, para poner el nombre de la ciudad que desee ver el clima

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const datosActuales = (lat,lon) =>`https://api.openweathermap.org/data/2.5/weath
 const datosHora = (lat,lon) => `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=6d38995b3a17ddfebcd1527c2a264c29&lang=es&units=metric&cnt=5`;
 const urlIconClima = (codigo) => `https://openweathermap.org/img/wn/${codigo}@2x.png`
 
-const inputCity = document.getElementById('city');
-const lat = document.getElementById('lat');
-const lon = document.getElementById('lon');
+const inputCity = document.querySelector('.ciudad');
+const btnBuscar = document.querySelector('.btn-buscar');
+const cerrar = document.querySelector('.close');
+const containerCambiarPais = document.querySelector('.container-cambiar-pais');
+const lat = document.getElementById('valor-lat');
+const lon = document.getElementById('valor-lon');
 const condicionClima = document.querySelector('.condicion-climatica');
 const temperatura = document.querySelector('.numero-tem-actual');
 const velocidaViento = document.querySelector('.velocidad-viento');
@@ -15,10 +18,13 @@ const presionAire = document.querySelector('.presion-de-aire');
 const iconTem = document.querySelector('.img')
 const containerClima3h = document.querySelector('.otros-datos-climaticos-clima-3h');
 const containerDatosClima3hCartas = document.querySelector('.datos-clima-3h-cartas ');
+const localizacionCiudad = document.querySelector('.localizacion-ciudad');
 
+//pinta las cartas del clima que muestra la condicion climatica cada tres horas
 function pintarCartasClima3h (arreglo) {
 
-arreglo.forEach(lista => {
+    containerClima3h.innerText = " ";
+    arreglo.forEach(lista => {
     const datosClima3hCartas = document.createElement('div');
     const fecha = document.createElement('p');
     const contianerIconClima = document.createElement('figure');
@@ -42,19 +48,14 @@ arreglo.forEach(lista => {
     datosClima3hCartas .append(fecha,contianerIconClima,containerTempGrados);
     containerClima3h.append(datosClima3hCartas);
 
-    console.log('fehca :',fecha)
-
 });
 
-
-
-
-}
+};
+// muestra la temperatura actual del pais seleccionado
 async function pronosticoActual (lat,lon){
     const res = await fetch(datosActuales(lat,lon));
     const data = await res.json();
     const icon = urlIconClima(data.weather[0].icon);
-    console.log('Pronostico Actual:',data)
 
     condicionClima.innerText = '';
     temperatura.innerText = '';
@@ -71,24 +72,19 @@ async function pronosticoActual (lat,lon){
     presionAire.innerText = data.main.pressure;
     iconTem.src = icon;
     
-
-    // icon (data.weather[0].icon);
-
-    console.log(icon)
 };
+
+// muestra la temperatura actual cada cada tres horas (por defecto muestra 5 temperaturas en lapsos de 3h cada una)
 async function pronosticoHora (lat,lon){
     const res = await fetch(datosHora(lat,lon));
     const data = await res.json();
-    console.log('pronostico hora :' ,data.list)
 
     pintarCartasClima3h (data.list);
     // var timestamp = data.list[dia].dt;  // Ejemplo de un timestamp en tiempo Unix
     // var dateObject = new Date(timestamp * 1000);  // JavaScript usa milisegundos
     // console.log(dateObject.toLocaleString());  // convertir a una cadena legible
 };
-
-
-
+// escribo el nombre de la ciudad y me devuelve la latitud y longitud de dicha ciudad
 async function ciudades (name){   // cosumo api de geolocalizacion
     const res = await fetch(city(name));
     const data = await res.json();
@@ -97,27 +93,44 @@ async function ciudades (name){   // cosumo api de geolocalizacion
     //     console.log('ciudad no valida');
     // }
 
-    // lat.innerText = '';
-    // lon.innerText = '';
-    // lat.innerText=data[0].lat;
-    // lon.innerText=data[0].lon;
+    lat.innerText = '';
+    lon.innerText = '';
+    localizacionCiudad.innerText = " ";
+    lat.innerText = data[0].lat;
+    lon.innerText = data[0].lon;
+    localizacionCiudad.innerText = data[0].name;
     pronosticoActual (data[0].lat,data[0].lon);
     pronosticoHora (data[0].lat,data[0].lon)
     
-    console.log('Ciudad :',data[0].name);
+  
+    console.log(data)
+    // console.log('lat:',lat, 'lon:',lon)
 };
 
+function showAside (){
+    containerCambiarPais.classList.add('show');
+};
+
+function closeAside (){
+    containerCambiarPais.classList.remove('show');
+}
+
+
+btnBuscar.addEventListener('click',showAside);
+cerrar.addEventListener('click',closeAside);
+
 // Detecta cuando dentro del input se preciona  Enter
-// inputCity.addEventListener('keydown',(event) =>{
-//     if (event.key === 'Enter') {         // Detectar si se preciono enter
-//         event.preventDefault();          // Evita que al priconar enter se mande el formulario
-//         inputCity.blur();               // quita el foco del input
-//         ciudades(event.target.value);
+inputCity.addEventListener('keydown',(event) =>{
+    if (event.key === 'Enter') {         // Detectar si se preciono enter
+        event.preventDefault();          // Evita que al priconar enter se mande el formulario
+        inputCity.blur();               // quita el foco del input
+        ciudades(event.target.value);
         
-//     }
-// });
+    }
 
-ciudades('medellin');
+    
+});
+
+ciudades('medellin')
 
 
-//  pronosticoHora();
